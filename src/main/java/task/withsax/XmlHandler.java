b4b3ee431681906f5df66d3f6f56b563648f7b91
Nekruz Rabiev <nekruz.rabiev@gmail.com>
2020-11-17 17:35:42 +0500

    Little changes

--- src/main/java/task/withsax/XmlHandler.java
+++ src/main/java/task/withsax/XmlHandler.java
@@ -3,27 +3,16 @@ package task.withsax;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.StringJoiner;
 
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-
-/**
- * Если все элементы с данными, то лучше использовать код из под комментариев. В
- * этом случае избавимся от HashSet-a
- **/
 
 public class XmlHandler extends DefaultHandler {
 
 	private String csvPath;
 	private String firstElement;
 	private String seperator;
-
-	// Названия тегов у которых есть данные
-	private HashSet<String> tags = null;
 
 	private FileWriter csvSource = null;
 	
@@ -38,22 +27,6 @@ public class XmlHandler extends DefaultHandler {
 	@Override
 	public void startDocument() throws SAXException {
 		try {
-			tags = new HashSet<String>(14);
-			tags.add("Id");
-			tags.add("Name");
-			tags.add("City");
-			tags.add("Country");
-			tags.add("IATA");
-			tags.add("ICAO");
-			tags.add("DST");
-			tags.add("Latitude");
-			tags.add("Longitude");
-			tags.add("Altitude");
-			tags.add("Offset");
-			tags.add("Timezone");
-			tags.add("Type");
-			tags.add("Source");
-
 			csvSource = new FileWriter(csvPath);
 		} catch (IOException e) {
 			e.printStackTrace();
